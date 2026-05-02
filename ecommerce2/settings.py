b2ae8e80519a769d9d8f41a84cd6e349008c7ca3
Shashikant625 <kant93634@hmail.com>
2026-05-02 14:31:15 +0530

    fix 500 error static

--- ecommerce2/settings.py
+++ ecommerce2/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # only for collectstatic in production
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'meadia'
